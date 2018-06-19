f16fae1639bc9d4b8ef89c1d98ff06aae3478200
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-06-19 10:30:31 -0700

    updated js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 
-var startGifs = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+var searchGifs = ["Spaceballs", "Final Fantasy 7", "Howls Moving Castle", "Mega-Man"];
 
-console.log(startGifs)
+console.log(searchGifs)
 $(document).ready(function () {
     
     // create and add buttons that user has entered in
     function renderButtons() {
-
-        $("#gifs-view").empty();
-
+        $("#view").empty();
+        
+        
         for (var i = 0; i < searchGifs.length; i++) {
             var a = $("<button>");
             a.addClass("gif");
@@ -18,19 +18,19 @@ $(document).ready(function () {
         }
     }
     $("#add-gif").on("click", function (event) {
-        let searchGifs = startGifs
-    console.log(searchGifs)
+        
         event.preventDefault();
         var userInput = $("#gif-input").val().trim();
         searchGifs.push(userInput);
         renderButtons();
     });
-
+    
     renderButtons();
+    
     // search gify for gifs and append to screen
     $(".gif").on('click', function () {
         let newGif = $(this).attr('data-name')
-        let queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + newGif + '&api_key=qwdsORm6eCZFU7evcJceLJPt51ve5HC5&tag&limit=10';
+        let queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + newGif + '&api_key=qwdsORm6eCZFU7evcJceLJPt51ve5HC5&tag&limit=5';
         console.log(queryURL)
         $.ajax({
             url: queryURL,
@@ -39,15 +39,15 @@ $(document).ready(function () {
             .then(function (response) {
                 let results = response.data
                 for (i = 0; i < results.length; i++) {
-                    if (results[i].rating !== 'R' && results[i] !== 'PG-13') {
-                    let gifDiv = $('<div id="gifs-view">')
+                    if (results[i].rating !== 'R') {
+                    let gifDiv = $('<div id="view">')
                     let rating = results[i].rating
                     let p = $('<p>').text('Rating: ' + rating)
                     let gifImage = $('<img>')
                     gifImage.attr('src', results[i].images.fixed_height.url)
-                    gifDiv.append(p)
-                    gifDiv.append(gifImage)
-                    $("#gifs-view").prepend(gifDiv)
+                    gifDiv.prepend(p)
+                    gifDiv.prepend(gifImage)
+                    $("#image").append(gifDiv)
                 }
                 }
             })
